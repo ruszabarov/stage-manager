afe545255df71633df75410ddfc05d8bb1f3c22c
Ruslan Zabarov <32499706+ruszabarov@users.noreply.github.com>
2024-04-27 20:10:09 -0400

    remove reloadConfig

--- init.lua
+++ init.lua
@@ -3,20 +3,6 @@
 -- LOAD VARIABLES
 monitorWatcher = nil
 
--- MY FUNCTIONS
--- function to reload init.lua when it changes
-function reloadConfig(files)
-    doReload = false
-    for _, file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-
 -- function that calls an applescript
 function monitorChangedCallback()
     local script = [[
@@ -32,10 +18,6 @@ function monitorChangedCallback()
     ]]
     hs.osascript.applescript(script)
 end
-
--- starts the pathwatcher to see if the init.lua changes in ~/.hammerspoon/
-initWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
 
 -- objects will be garbage collected if they aren't in explicit vars
 -- Starts the screen watcher, making it so fn is called each time the screen arrangement changes
